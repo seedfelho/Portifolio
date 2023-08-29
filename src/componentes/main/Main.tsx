e4fc5085f1b48431183d715264e0ac1a91454437
seedfelho <sergio.filho2@estudante.ifms.edu.br>
2023-08-29 08:31:33 -0400

    arrumando main e footer

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -2,7 +2,11 @@ import './Main.css'
 
 function Main(){
     return(
-        <main></main>
+        <main>
+            <div className="containerMain">
+                <a className="textMain">Ola, me chamo Sergio, sou um aluno do curso tecnico em informatica para internet que ao longo do curso desenvolveu alguns projetos. O objetivo deste portifolio é mostrar alguns deles</a>
+            </div>
+        </main>
     )
 }
 
