f81eae1e697921dd8cb044822fa3bc9e09b4ac29
seedfelho <131921797+seedfelho@users.noreply.github.com>
2023-09-19 10:58:55 -0400

    barra de pesquisa funcional para projetos feitos

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,8 +1,43 @@
+import { useState } from 'react';
 import './Main.css'
+import Projeto from '../projeto/Projeto';
+
+type ProjetoType = {
+    id: number,
+    titulo: string,
+    resumo:string,
+    imagem: string
+}
 
 function Main(){
+    const [texto,setTexto] = useState("")
+
+    const projetos:ProjetoType[] = [
+        {
+            id:1,
+            titulo: "Barbie",
+            resumo:"Site desenvolvido na materia de Framework 2 no 6° periodo do curso tecnico em informatica para internet",
+            imagem:"/barbie.png"
+        },
+        {
+            id:2,
+            titulo: "Gameficação na educação",
+            resumo:"O projeto tem como objetivo desenvolver um site para os professores poderem acessar e ver ferramentas que ele pode ultilizar para deixar o estudar mais divertido",
+            imagem:"/gamificacao.png"
+        },
+        {
+            id:3,
+            titulo: "OBI",
+            resumo:"Projeto com objetivo de preparar os alunos para a OBI(olímpida brasileira de informatica) e estuda mais sobre programação",
+            imagem:"/logoObi.png"
+        }
+]
+    function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
+        // console.log(e.target.value)
+        setTexto(e.target.value)
+    }
     return(
-        <main>
+        <main className="main_projeto">
             <div className="containerMain">
                 <p className="textMain">Ola, me chamo Sergio, sou um aluno do curso tecnico em informatica para internet que ao longo do curso desenvolveu alguns projetos. O objetivo deste portifolio é mostrar alguns deles</p>
             </div>
@@ -17,6 +52,16 @@ function Main(){
                         </li>
                     </ul>
                 </nav>
+            </div>
+            <div className="pesquisa">
+            <p>Buscar filme</p>
+                <input className="barrapesquisa" type="text" onChange={mudaTexto}/>
+                <div>
+                    <p className="texto_digitado">Pesquisa: {texto}</p>    
+                </div>
+            </div>
+            <div className="content_projeto">
+                { projetos.filter((p) => p.titulo.toLowerCase().includes(texto)).map( (pro) => <Projeto key = { pro.id } titulo={ pro.titulo } resumo={ pro.resumo } imagem={pro.imagem}/>) }
             </div>
         </main>
     )
---
+++ src/componentes/projeto/Projeto.tsx
@@ -0,0 +1,21 @@
+import "./Projeto.css";
+type ProjetoProps={
+    titulo:string,
+    imagem:string,
+    resumo:string
+}
+function Projeto(props:ProjetoProps){
+    return(
+        <div className="projeto_content">
+            <div className="img_projeto">
+                <img className="imgbar" src={props.imagem} alt=""/>
+            </div>
+            <div className="titulofoto">
+                <h1 className="h1-titulo">{props.titulo}</h1>
+                <p className="sinopse">{props.resumo}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Projeto
